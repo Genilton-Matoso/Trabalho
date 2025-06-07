6783ea7125db26fc2fa8ffc95a19e989a9ff7c80
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-06-07 20:36:47 -0300

    first commit

---
+++ scrip.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+  const mensagem = "✨ Tenha um dia incrível e nunca desista dos seus sonhos!";
+  const divMensagem = document.getElementById("mensagem");
+  divMensagem.innerHTML = mensagem;}
+
+
+
+
